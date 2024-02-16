dc1e9f7d765c2b41f9778eeb593a65a3479eb2c9
Egor Volkov Home <gole00201@gmail.com>
2024-02-16 23:57:46 +0300

    DFP test

--- test_dfp/test_dfp.ino
+++ test_dfp/test_dfp.ino
@@ -5,32 +5,22 @@ SoftwareSerial mySerial(9, 8);
 // 9 - RX, 8 - TX
 
 void setup () {
-	Serial.begin(9600);
+	Serial.begin (9600);
     // открываем порт для консоли
-	mySerial.begin(9600);
+	mySerial.begin (9600);
     // открываем софт-порт
-	mp3_set_serial(mySerial);
+	mp3_set_serial (mySerial);
     // задаем порт для плеера
-	mp3_set_volume(30);
+	mp3_set_volume (30);
     //устанавливаем уровень громкости
 }
 
 void loop () {
-	mp3_play (1001);
+	mp3_play (3002);
     Serial.print("Play sound");
 	delay (6000);
-    mp3_play (1002);
-    Serial.print("Play sound");
-	delay (6000);
-    mp3_play (1003);
-    Serial.print("Play sound");
-	delay (6000);
-    mp3_play (1004);
-    Serial.print("Play sound");
-	delay (6000);
-    mp3_play (1005);
-    Serial.print("Play sound");
-	delay (6000);
+
+	mp3_next ();
     // включаем следующий файл
 	delay (6000);
 }
